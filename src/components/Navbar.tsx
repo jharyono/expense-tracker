1ea9a84ebfdbe0342124ba0b5d3247d103b12dca
Joni Haryono <jharyono@gmail.com>
2024-07-03 14:54:04 +0700

    build apps

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <nav className="flex justify-center h-10 my-2 items-center">
+      <h1 className="text-3xl">Personal Expense Tracker</h1>
+    </nav>
+  )
+}
+
+export default Navbar
